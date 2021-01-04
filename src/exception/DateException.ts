1dfc32b7641448deab2169ce3d3f72e9500927fa
Damien <damien.jisseau@outlook.fr>
2021-01-04 21:14:36 +0100

    Création d'une méthode adaptant la date au format adéquat

--- src/exception/DateException.ts
+++ src/exception/DateException.ts
@@ -7,4 +7,14 @@ export default class DateException extends Error{
         const reg = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
         return (reg.test(date.toLowerCase().trim()));
     }
+
+    // Set current date to yyyy-MM-dd format
+    public static formatDate(date: Date): string{
+        let dd = String(date.getDate()).padStart(2, '0');
+        let mm = String(date.getMonth() + 1).padStart(2, '0');
+        let yyyy = date.getFullYear();
+        let currentDate = yyyy + '-' + mm + '-' + dd;
+
+        return currentDate;
+    }
 }
