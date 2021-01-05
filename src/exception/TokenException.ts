14bb088867c5f0b554d91c677b0e8c8b8073b680
Damien <damien.jisseau@outlook.fr>
2021-01-05 15:22:09 +0100

    Création de la méthode isTokenValid()
    Cette méthode permet de vérifier si le bearer token est présent, s'il est valide, et s'il n'est pas dans la blacklist

--- src/exception/TokenException.ts
+++ src/exception/TokenException.ts
@@ -1,3 +1,6 @@
+import { verify } from "jsonwebtoken";
+import Blacklist from "../models/Blacklist";
+
 export default class TokenException extends Error{
     constructor(){
         super('Token is not valid!');
@@ -5,5 +8,17 @@ export default class TokenException extends Error{
 
     static split(token: string): string {
         return token.split('Bearer ').join('');
+    }
+
+    // Check if a token exists and is valid
+    static async isTokenValid(headerAuthorization: any): Promise<boolean>{
+        if (headerAuthorization && verify(TokenException.split(headerAuthorization), <string>process.env.JWT_KEY)){        
+            // Check if the current token is blacklisted (if the user logged out before the token expiration time)          
+            const isTokenBlacklisted = await Blacklist.isExiste(TokenException.split(headerAuthorization));
+            console.log(isTokenBlacklisted);
+            if (!isTokenBlacklisted) return true;
+        }
+
+        return false;
     }
 }
