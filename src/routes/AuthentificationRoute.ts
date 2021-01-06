a1d0a28449b193535d8a1d0c4f776900edd96c0f
Damien <damien.jisseau@outlook.fr>
2021-01-06 22:54:27 +0100

    Adding comments to AuthentificationRoute

--- src/routes/AuthentificationRoute.ts
+++ src/routes/AuthentificationRoute.ts
@@ -5,12 +5,16 @@ import { Request, Response } from 'express';
 
 const route: Router = Router();
 
+// Main route
 route.get('/', (req: Request, res: Response) => {
     try{ return res.end('<h1>Welcome to Zoubify!</h1>'); }
     catch{ return res.end('<h1>404 not found!</h1>'); }
 })
 
+// Sign Up a new user
 route.post('/register', registerMidd, AuthController.register);
+
+// Sign In an existing user
 route.post('/login', loginMidd, AuthController.login);
 
 export { route as AuthentificationRoute }
