4718e71eb16ff3e3cd8476d7fd7d2cea7c30d173
Damien <damien.jisseau@outlook.fr>
2021-01-07 10:33:47 +0100

    Page index.ts

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { config } from "dotenv";
+import bodyParser from "body-parser";
+import express from "express";
+import cors from "cors";
+import { AuthentificationRoute } from "./src/routes/AuthentificationRoute";
+import { UserRoute } from "./src/routes/UserRoute";
+import { SongRoute } from "./src/routes/SongRoute";
+
+config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Main & authentification routes.
+app.use('/', AuthentificationRoute);
+
+// User routes.
+app.use('/user', UserRoute);
+
+// Songs routes.
+app.use('/songs', SongRoute);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server run to http://localhost:${process.env.PORT}`);
+});
